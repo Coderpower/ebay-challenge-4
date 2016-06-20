64296994d7cb97fe4fe721c7b5cbc88afd007434
Julien Sarazin <julien.sarazin@me.com>
2016-06-20 20:05:51 +0200

    updated fixtures

--- sources/delivery.js
+++ sources/delivery.js
@@ -1,5 +1,4 @@
 function notify(driver , sellers){
-
 };
 
 module.exports = notify;
--- test/delivry.specs.js
+++ test/delivry.specs.js
@@ -17,13 +17,13 @@ suite('Package Delivery', function(){
         **/
 
         var driver = require('./data/scenario-1/driver.json');
-        var customers = require('./data/scenario-1/customers.json');
+        var sellers = require('./data/scenario-1/sellers.json');
         var expectations = [
             "07479caa-9a45-4eeb-8717-a5107cec2b71",
             "777e1c4d-e695-423e-ac46-751ef7083e8c"
         ];
 
-        expect(prepareDelivery(driver, customers)).to.eql(expectations);
+        expect(prepareDelivery(driver, sellers)).to.eql(expectations);
     });
 
     test("Validating itinary two.", function () {
@@ -41,13 +41,13 @@ suite('Package Delivery', function(){
         **/
 
         var driver = require('./data/scenario-2/driver.json');
-        var customers = require('./data/scenario-2/customers.json')
+        var sellers = require('./data/scenario-2/sellers.json')
         var expectations = [
             "bc488845-8f9d-42fa-a0ba-2fff7971fc76",
             "62bedee5-8ac3-4431-ac16-1ca158791aa1"
         ]
 
-        expect(prepareDelivery(driver, customers)).to.eql(expectations);
+        expect(prepareDelivery(driver, sellers)).to.eql(expectations);
     });
 
     test("Validating itinary three.", function () {
@@ -65,14 +65,13 @@ suite('Package Delivery', function(){
         **/
 
         var driver = require('./data/scenario-3/driver.json');
-        var customers = require('./data/scenario-3/customers.json')
+        var sellers = require('./data/scenario-3/sellers.json')
         var expectations = [
             "a37d22e6-8883-43ac-bf92-c97e774e11ec",
             "27e39825-c2f1-4e80-a3a4-148abec98dde",
             "78fa4f3a-c041-4884-8892-617eb9cc6e06"
         ]
 
-        expect(prepareDelivery(driver, customers)).to.eql(expectations);
+        expect(prepareDelivery(driver, sellers)).to.eql(expectations);
     });
-
 });
